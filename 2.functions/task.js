2c02e70b0f7767d36e06dac496657c7802f2a068
Rustam Tlepberghenov <rustsik@gmail.com>
2022-07-25 21:41:28 +0300

    Update task.js

--- 2.functions/task.js
+++ 2.functions/task.js
@@ -1,24 +1,27 @@
+"use stict";
 // Задание 1
 function getArrayParams(arr) {
   let min, max, avg;
   sum = 0;
-  min = Infinity;
-  max = -Infinity;
-  for(let i = 0; i > arr.length; i++){
-   console.log(arr.length[i]);
+  min = 0;
+  max = 0;
+  for(let i = 0; i < arr.length; i++){
     if(i > max){
       max = i;
     }else if(i < min){
       min = i;
     }
+    console.log(`i: ${i}`);
     sum + i;
+    console.log(`sum: ${sum}`);
   }
   avg = sum / arr.length;
   avg.toFixed(2);
-  console.log(avg);
+  console.log(`avg: ${avg}`);
+  console.log(`sum: ${sum}`);
   return { min: min, max: max, avg: avg };
 }
-//почему-то avg выдает 0..
+//почему-то avg и sum выдают 0..
 
 // Задание 2
 function worker(arr) {
